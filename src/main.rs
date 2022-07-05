e009c063a81bba7ae7802f1f9cad4b4c053fbbff
Anders Arnholm <anders@arnholm.se>
2022-07-05 06:44:20 +0200

    Fix the mesh triangle order
    Make triangle the other way around so normals are up
    and you see the mesh looking down on it as a nice terrain

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use bevy::pbr::wireframe::{Wireframe, WireframeConfig, WireframePlugin};
 use bevy::prelude::*;
 use bevy::render::mesh::{Indices, PrimitiveTopology};
 
@@ -6,166 +7,119 @@ fn main() {
         .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
+        .add_plugin(WireframePlugin)
         .add_system(rotate_cam)
         .run();
 }
 
-fn create_terrain() -> Mesh {
+struct HeightMap {
+    width: u16,
+    max_height: f64,
+    min_height: f64,
+    map: Vec<f64>,
+}
+
+fn load_height_map() -> HeightMap {
+    // Terrain mesh
+    let _map_6x6 = HeightMap {
+        width: 6,
+        max_height: 0.3,
+        min_height: -0.1,
+        map: vec![
+            -0.1, 0.1, 0.1, 0.1, 0.1, 0.1,
+            -0.1, 0.2, 0.2, 0.2, 0.2, 0.1,
+            -0.1, 0.2, 0.1, 0.1, 0.3, 0.1,
+            -0.1, 0.2, -0.1, 0.1, 0.3, 0.1,
+            -0.1, 0.2, 0.2, 0.2, 0.3, 0.1,
+            -0.1, 0.1, 0.1, 0.1, 0.1, 0.1,
+        ],
+    };
+    let _map_3x3 = HeightMap {
+        width: 3,
+        max_height: 8.,
+        min_height: 0.,
+        map: vec![
+            0., 1., 0.,
+            3., 4., 5.,
+            6., 5., 8.,
+        ]
+    };
+    let _map_2x2 = HeightMap {
+        width: 2,
+        max_height: 0.2,
+        min_height: -0.1,
+        map: vec![
+            -0.1, 0.1,
+            -0.2, 0.2,
+        ]
+    };
+    let _map_4x4 = HeightMap {
+        width: 4,
+        max_height: 1.0,
+        min_height: 0.,
+        map: vec![
+            0.0, 1.0, 0.0, 1.0,
+            1.0, 0.0, 1.0, 0.0,
+            0.0, 1.0, 0.0, 1.0,
+            1.0, 0.0, 1.0, 0.0,
+        ]
+    };
+    let _map_5x5 = HeightMap {
+        width: 5,
+        max_height: 0.1,
+        min_height: 0.1,
+        map: vec![
+            0.1, 0.1, 0.1, 0.1, 0.1,
+            0.1, 0.1, 0.1, 0.1, 0.1,
+            0.1, 0.1, 0.1, 0.1, 0.1,
+            0.1, 0.1, 0.1, 0.1, 0.1,
+            0.1, 0.1, 0.1, 0.1, 0.1,
+        ]
+    };
+    _map_6x6
+}
+
+fn create_simple_terrain(height_map: &HeightMap) -> Mesh {
     let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
-    let vertices = vec![
-        [0f32, -0.1, 0f32],
-        [0f32, -0.1, 1f32],
-        [0f32,  0.1, 2f32],
-        [0f32,  0.1, 3f32],
-        [0f32, -0.1, 4f32],
-        [0f32, -0.1, 5f32],
+    let heights = height_map;
+    let size = height_map.width;
+    let mut vertices: Vec<[f32; 3]> = Vec::with_capacity(heights.map.len());
+    let mut normals: Vec<[f32; 3]> = Vec::with_capacity(heights.map.len());
+    let mut uvs: Vec<[f32; 2]> = Vec::with_capacity(heights.map.len());
+    let mut indices_vector: Vec<u16> = Vec::new();
 
-        [1f32,  0.0, 0f32],
-        [1f32,  0.1, 1f32],
-        [1f32,  0.5, 2f32],
-        [1f32,  0.5, 3f32],
-        [1f32,  0.1, 4f32],
-        [1f32,  0.0, 5f32],
+    let mut x = 0;
+    let mut z = 0;
 
-        [2f32,  0.0, 0f32],
-        [2f32,  0.1, 1f32],
-        [2f32,  0.6, 2f32],
-        [2f32,  0.6, 3f32],
-        [2f32,  0.1, 4f32],
-        [2f32,  0.0, 5f32],
+    for height in &heights.map {
+        vertices.push([x as f32, *height as f32, z as f32]);
+        normals.push([0f32, 0f32, 1f32]);
+        uvs.push([x as f32, z as f32]);
+        x += 1;
+        if x == size {
+            x = 0;
+            z += 1;
+        }
+    }
 
-        [3f32,  0.2, 0f32],
-        [3f32,  0.1, 1f32],
-        [3f32,  0.4, 2f32],
-        [3f32,  0.4, 3f32],
-        [3f32,  0.1, 4f32],
-        [3f32,  0.2, 5f32],
+    let l = (heights.map.len() - size as usize - 1) as u16;
+    for a in 0..l {
+        if !(a % size as u16 == size as u16 - 1) {
+            indices_vector.push(a);
+            indices_vector.push(a + 1u16 + (size as u16));
+            indices_vector.push(a + 1u16);
 
-        [4f32, -0.1, 0f32],
-        [4f32,  0.2, 1f32],
-        [4f32,  0.2, 2f32],
-        [4f32,  0.3, 3f32],
-        [4f32,  0.2, 4f32],
-        [4f32, -0.1, 5f32],
-
-        [5f32, -0.1, 0f32],
-        [5f32, -0.1, 1f32],
-        [5f32,  0.3, 2f32],
-        [5f32,  0.3, 3f32],
-        [5f32, -0.1, 4f32],
-        [5f32, -0.1, 5f32],
-
-    ];
-    let normals = vec![
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-        [0f32, 0f32, 1f32],
-
-    ];
-    let uvs = vec![
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-        [0.0000, 0.0000],
-    ];
-
-    /*
-          0  1  2  3  4  5
-          6  7  8  9 10 11
-         12 13 14 15 16 17
-         18 19 20 21 22 23
-         24 25 26 27 28 29
-         30 31 32 33 34 35
-     */
-    let indices = Indices::U16(vec![
-         0, 1, 7,  1, 2, 8,  2, 3, 9,  3, 4,10,  4, 5,11,
-         0, 7, 6,  1, 8, 7,  2, 9, 6,  3,10, 9,  4,11,10,
-
-         6, 7,13,  7, 8,14,  8, 9,15,  9,10,16, 10,11,17,
-         6,13,12,  7,14,13,  8,15,14,  9,16,15, 10,17,16,
-
-        12,13,19, 13,14,20, 14,15,21, 15,16,22, 16,17,23,
-        12,19,18, 13,20,19, 14,21,20, 15,22,21, 16,23,22,
-
-        18,19,25, 19,20,26, 20,21,27, 21,22,28, 22,23,29,
-        18,25,24, 19,26,25, 20,27,26, 21,28,27, 22,29,28,
-
-        24,25,31, 25,26,32, 26,27,33, 27,28,34, 28,29,35,
-        24,31,30, 25,32,31, 26,33,32, 27,34,33, 28,35,34,
-
-
-
-    ]);
-
+            indices_vector.push(a);
+            indices_vector.push(a + 0u16 + (size as u16));
+            indices_vector.push(a + 1u16 + (size as u16));
+        }
+    }
+    let indices = Indices::U16(indices_vector);
     mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, vertices);
     mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
     mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
     mesh.set_indices(Some(indices));
+
     mesh
 }
 
@@ -173,7 +127,7 @@ fn create_terrain() -> Mesh {
 fn rotate_cam(mut query: Query<&mut Transform, With<Camera>>, time: Res<Time>) {
     let hsize = (5 / 2) as f32;
     for mut transform in query.iter_mut() {
-        let alpha = (time.seconds_since_startup() as f32 / 2.);
+        let alpha = time.seconds_since_startup() as f32 / 2.;
         *transform = Transform::from_xyz(
             hsize + alpha.cos() * hsize,
             3.,
@@ -186,26 +140,33 @@ fn rotate_cam(mut query: Query<&mut Transform, With<Camera>>, time: Res<Time>) {
 /// set up a simple 3D scene
 fn setup(
     mut commands: Commands,
+    mut wireframe_config: ResMut<WireframeConfig>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // Terrain mesh
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(create_terrain()),
-        material: materials.add(Color::rgb(0.1, 0.7, 0.3).into()),
-        transform: Transform::from_xyz(-2.5, 0.0, -2.5),
-        ..default()
-    });
+    wireframe_config.global = false;
     // water plane
     commands.spawn_bundle(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Plane { size: 1500.0 })),
-        material: materials.add(Color::rgba(0.1, 0.1, 0.7, 0.3).into()),
-        transform: Transform::from_xyz(0.0, -0.0, 0.0),
+        material: materials.add(Color::rgba(0.1, 0.1, 0.7, 0.2).into()),
+        transform: Transform::from_xyz(0.0, 0., 0.0),
         ..default()
     });
+
+    let height_map = load_height_map();
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(create_simple_terrain(&height_map)),
+        material: materials.add(Color::rgb(0.1, 0.7, 0.3).into()),
+        transform: Transform::from_xyz(-(height_map.width as f32 / 2.),
+                                       0.,
+                                       -(height_map.width as f32 / 2.)),
+        ..default()
+    })
+        .insert(Wireframe)
+    ;
     // cube
     commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 0.4 })),
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 0.1 })),
         material: materials.add(Color::rgb(0.3, 0.3, 0.3).into()),
         transform: Transform::from_xyz(0.0, 0.5, 0.0),
         ..default()
@@ -225,4 +186,115 @@ fn setup(
         transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{create_simple_terrain, HeightMap};
+    use bevy::prelude::*;
+    use bevy::render::mesh::Indices::U16;
+    use bevy::render::mesh::VertexAttributeValues;
+
+
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    /// Make sure a simple 2x2 height map generates correct
+    ///
+    /// The test sets up a height map like this:
+    ///      0  1
+    ///      -  -
+    ///  0|  0  1
+    ///  1|  2  3
+    ///
+    /// the verifies the resulting positions and indices for the mesh
+    fn make_simple_terrain_2x2() {
+        let _map_2x2 = HeightMap {
+            width: 2,
+            max_height: 3.,
+            min_height: 0.,
+            map: vec![
+                0.,
+                1.,
+                2.,
+                3.,
+            ],
+        };
+        let mesh = create_simple_terrain(&_map_2x2);
+        assert_eq!(mesh.count_vertices(), 4);
+        let positions = mesh.attribute(Mesh::ATTRIBUTE_POSITION).unwrap();
+        dbg!("{}", positions);
+        let expected_pos = VertexAttributeValues::from(vec![
+            [0., 0., 0.],
+            [1., 1., 0.],
+            [0., 2., 1.],
+            [1., 3., 1.],
+        ]);
+        assert_eq!(positions.get_bytes(), expected_pos.get_bytes());
+
+        if let U16(bytes) = mesh.indices().unwrap() {
+            dbg!("{}", bytes);
+            assert_eq!(*bytes, vec![
+                0u16, 3u16, 1u16,
+                0u16, 2u16, 3u16,
+            ]);
+        }
+    }
+
+    #[test]
+    /// Make sure a simple 3x3 height map generates correct
+    ///
+    /// The test sets up a height map like this:
+    ///      0  1  2
+    ///      -  -  -
+    ///  0|  0  1  2
+    ///  1|  3  4  5
+    ///  2|  6  7  8
+    ///
+    /// the verifies the resulting positions and indices for the mesh
+    fn make_simple_terrain_3x3() {
+        let _map_3x3 = HeightMap {
+            width: 3,
+            max_height: 8.,
+            min_height: 0.,
+            map: vec![
+                0., 1., 2.,
+                3., 4., 5.,
+                6., 7., 8.,
+            ],
+        };
+        let mesh = create_simple_terrain(&_map_3x3);
+        assert_eq!(mesh.count_vertices(), 9);
+        let positions = mesh.attribute(Mesh::ATTRIBUTE_POSITION).unwrap();
+        dbg!("{}", positions);
+        let expected_pos = VertexAttributeValues::from(vec![
+            [0., 0., 0.],
+            [1., 1., 0.],
+            [2., 2., 0.],
+            [0., 3., 1.],
+            [1., 4., 1.],
+            [2., 5., 1.],
+            [0., 6., 2.],
+            [1., 7., 2.],
+            [2., 8., 2.],
+        ]);
+        assert_eq!(positions.get_bytes(), expected_pos.get_bytes());
+
+        if let U16(bytes) = mesh.indices().unwrap() {
+            dbg!("{}", bytes);
+            assert_eq!(*bytes, vec![0u16, 4u16, 1u16,
+                                    0u16, 3u16, 4u16,
+                                    1u16, 5u16, 2u16,
+                                    1u16, 4u16, 5u16,
+                                    3u16, 7u16, 4u16,
+                                    3u16, 6u16, 7u16,
+                                    4u16, 8u16, 5u16,
+                                    4u16, 7u16, 8u16,
+            ]);
+        }
+    }
 }
